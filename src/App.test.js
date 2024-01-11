e365b0c9a396caa1efa45c88b07e808da61f1b38
Parker <parkermdinsmore@gmail.com>
2024-01-11 12:32:11 -0300

    small update

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 
-import App from './App';
+import App from "./App";
 
 const mockName = "guy";
 const mockEmail = "guy@fieri.com";
 
-test('can recieve a new user and show it on a list', async () => {
+test("can recieve a new user and show it on a list", async () => {
   render(<App />);
-  
-  const nameInput = screen.getByRole("textbox", { name: /name/i })
-  const emailInput = screen.getByRole("textbox", { name: /email/i })
 
-    // Simulate typing in a name
-    await user.click(nameInput);
-    await user.keyboard(mockName);
-  
-    // Simulate typing in an email
-    await user.click(emailInput);
-    await user.keyboard(mockEmail);
-  
-    // Find the button
-    const button = screen.getByRole("button");
-  
-    // Simulate clicking the button
-    await user.click(button);
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
 
-    // screen.debug()
-    const name = screen.getByRole("cell", { name: mockName });
-    const email = screen.getByRole("cell", { name: mockEmail });
+  // Simulate typing in a name
+  await user.click(nameInput);
+  await user.keyboard(mockName);
 
-    expect(name).toBeInTheDocument();
-    expect(email).toBeInTheDocument();
+  // Simulate typing in an email
+  await user.click(emailInput);
+  await user.keyboard(mockEmail);
+
+  // Find the button
+  const button = screen.getByRole("button");
+
+  // Simulate clicking the button
+  await user.click(button);
+
+  // screen.debug()
+  const name = screen.getByRole("cell", { name: mockName });
+  const email = screen.getByRole("cell", { name: mockEmail });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
